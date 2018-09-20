c9dfdda5bc46542eee9fc520672d6e92f8381346
miller45 <gmse45forse@googlemail.com>
2018-09-20 18:54:07 +0200

    rework

--- arduinocsc.ino
+++ arduinocsc.ino
@@ -1,153 +1,286 @@
 //#define ALLEVENTS
+#define SERIALDBG
+#define MINTERVAL 3100
 
-#include <CurieBLE.h>
+#define FACTORYRESET_ENABLE
+#define MINIMUM_FIRMWARE_VERSION   "0.7.0"
 
-//CSC = Cycling Speed and Cadence
-#define FLAGS_SIZE 1
-#define WHEEL_REV_SIZE 4
-#define CRANK_REV_SIZE 2
-#define TIME_EVENT_SIZE 2
+//bluefruit stuff
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+#include <SoftwareSerial.h>
+#endif
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BLEGatt.h"
+#include "bluefruitconfig.h"
+
+
+//conenience defines
+#define ERROR_IF_ZERO(VARI,MSG) if(VARI==0) { error(F(MSG));  }
+#define ERROR_IF_NOT(VARI,MSG) if(! VARI) { error(F(MSG));  }
+#ifdef SERIALDBG
+#define SERIALPRINT(VARI) Serial.print(VARI)
+#define SERIALPRINTLN(VARI) Serial.println(VARI)
+#else
+#define SERIALPRINT(VARI) /*(VARI)*/
+#define SERIALPRINTLN(VARI) /*(VARI)*/
+#endif
+
+volatile uint8_t *ledport;
+volatile uint8_t ledbit;
+
+#ifdef ALLEVENTS
+const unsigned char featFlagsArray[2] = {  CSC_CRANK_REV_PRESENT | CSC_WHEEL_REV_PRESENT, 0 };
+#else
+const unsigned char featFlagsArray[2] = {  CSC_WHEEL_REV_PRESENT, 0 };
+#endif
+const unsigned char locationArray[1] = {  6 };
+uint8_t advdataArray[9] = { 0x02, 0x01, 0x06, 0x05, 0x02, 0x09, 0x18, 0x0a, 0x18 };
+#define ADVLOWIDPOS 5
+#define ADVHIGHIDPOS 6
+
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+Adafruit_BLEGatt gatt(ble);
+
+
+// A small helper for showing error msg
+void error(const __FlashStringHelper*err) {
+
+  SERIALPRINT(err);
+
+  while (1) {
+    blinkLED(4);
+    delay(1000);
+  }
+}
+
 #ifdef ALLEVENTS
 #define CSCMSIZE FLAGS_SIZE+WHEEL_REV_SIZE+TIME_EVENT_SIZE+CRANK_REV_SIZE+TIME_EVENT_SIZE
 #else
 #define CSCMSIZE FLAGS_SIZE+WHEEL_REV_SIZE+TIME_EVENT_SIZE
 #endif
 
-#define CSC_WHEEL_REV_PRESENT 1
-#define CSC_CRANK_REV_PRESENT 2
-
-#define MINTERVAL 3000
 
 #define INTERRUPTPIN  2
 
-BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're programming)
-BLEService cscService("1816"); // BLE CSCe Service
-
-BLECharacteristic cscFeaturesChar ("2A5C", BLERead, 2); //16bit
-BLECharacteristic cscMeasureChar ("2A5B",  BLENotify, CSCMSIZE); //bleread not needed
-BLECharCharacteristic cscSensLocChar ("2A5D",  BLERead);
-BLECharacteristic cscSCControlPointChar ("2A55",  BLEWrite | BLEIndicate, 255);
-
-//BLEService devInfoService("180A"); // BLE CSCe Service
-//BLECharacteristic manuNameChar ("2A29", BLERead, 3);
-//docs see https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.cycling_speed_and_cadence.xml
-
-//Services
-// Cycling Speed and Cadence  0x1816
-//Characteristics
-//CSC Feature 0x2A5C
-//CSC Measurement 0x2A5B
+int32_t cscServiceId;
+int32_t cscMeasureCharId;
+int32_t cscFeaturesCharId;
+int32_t cscSensorLocCharId;
+int32_t cscServicePointCharId;
 
 volatile uint32_t currWheelPos = 0;  // last heart rate reading from analog input
 boolean isodd = false;
-uint16_t currCrankPos = 0;  // last heart rate reading from analog input
 uint16_t wheelTime = 0;
+uint16_t currCrankPos = 0;  // last heart rate reading from analog input
 long previousMillis = 0;  // last time the heart rate was checked, in ms
 
-volatile byte slice=0;
-volatile bool updating=false;
+volatile byte slice = 0;
+volatile bool updating = false;
+volatile bool is_central_connected = false;
+bool last_is_central_connected;
 
 void setup() {
-  pinMode(INTERRUPTPIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), do_wcount, FALLING);
+  initLEDPort();
+  blinkLED(1);
+#ifdef SERIALDBG
+  Serial.begin(9600);    // initialize serial communication
+#endif
+  SERIALPRINT("Start: ");
+  SERIALPRINT(__DATE__);
+  SERIALPRINT(" ");
+  SERIALPRINTLN(__TIME__);
+
+  pinMode(INTERRUPTPIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), doWheelCount, FALLING);
+
+  initBluetoothServices();
 
   pinMode(LED_BUILTIN, OUTPUT);
   blinkLED(3);
-  Serial.begin(9600);    // initialize serial communication
-  Serial.println(CSCMSIZE);
-  /* Set a local name for the BLE device
-     This name will appear in advertising packets
-     and can be used by remote devices to identify this BLE device
-     The name can be changed but maybe be truncated based on space left in advertisement packet */
-  blePeripheral.setLocalName("CSCSketch");
-  blePeripheral.setAdvertisedServiceUuid(cscService.uuid());
 
-  blePeripheral.addAttribute(cscService);
-
-  //configure features chartecriscigtsc
-#ifdef ALLEVENTS
-  const unsigned char featFlagsArray[2] = {  CSC_CRANK_REV_PRESENT | CSC_WHEEL_REV_PRESENT, 0 };
-#else
-  const unsigned char featFlagsArray[2] = {  CSC_WHEEL_REV_PRESENT, 0 };
-#endif
-
-  blePeripheral.addAttribute(cscMeasureChar);
-
-  blePeripheral.addAttribute(cscFeaturesChar);
-  cscFeaturesChar.setValue(featFlagsArray, 2);
-
-  blePeripheral.addAttribute(cscSensLocChar);
-  cscSensLocChar.setValue(6);
-
-  blePeripheral.addAttribute(cscSCControlPointChar);
-  cscSCControlPointChar.setEventHandler(BLEWritten, cscSCControlPointCharWritten);
-
-  //manu uinfo
-  // blePeripheral.addAttribute(devInfoService);
-  //const unsigned char manuArray[3] = { 0x41,0x42,0x43 };
-  // blePeripheral.addAttribute(manuNameChar);
-  // manuNameChar.setValue(manuArray,3);
-
-
-  /* Now activate the BLE device.  It will start continuously transmitting BLE
-     advertising packets and will be visible to remote BLE central devices
-     until it receives a new connection */
-  blePeripheral.begin();
-  Serial.print("CSC Bluetooth device active, waiting for connections...");
-#ifdef ALLEVENTS
-  Serial.print("ALLEVENTS Mode ");
-  Serial.println(CSCMSIZE);
-#else
-  Serial.print("ONLY wheel events Mode ");
-  Serial.println(CSCMSIZE);
-#endif
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // listen for BLE peripherals to connect:
-  BLECentral central = blePeripheral.central();
-
-  // if a central is connected to peripheral:
-  if (central) {
-    Serial.print("Connected to central: ");
-    // print the central's MAC address:
-    Serial.println(central.address());
-    // turn on the LED to indicate the connection:
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    // check the CSC measurement every 200ms
-    // as long as the central is still connected:
-    while (central.connected()) {
-      if(updating){
-        delay(1); //if value is currentliy updating wait a bit
-      }
-      long currentMillis = millis();
-      // if MINTERVAL ms have passed, check the csc measurement:
-      if (currentMillis - previousMillis >= MINTERVAL) {
-        previousMillis = currentMillis;
-        updateCSC(currentMillis) ;
-      }
+  ble.update(200);
+  if (last_is_central_connected != is_central_connected) {
+    if (is_central_connected) {
+      SERIALPRINTLN( F("Connected") );
+      readAndPrintPeerAddress();
+    } else {
+      SERIALPRINTLN( F("Disconnected") );
     }
-    // when the central disconnects, turn off the LED:
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.print("Disconnected from central: ");
-    Serial.println(central.address());
+    last_is_central_connected = is_central_connected;
+  }
+  if (is_central_connected)
+  {
+    long currentMillis = millis();
+    // if MINTERVAL ms have passed, check the csc measurement:
+    if (currentMillis - previousMillis >= MINTERVAL) {
+      previousMillis = currentMillis;
+      updateCSCviaBLE(currentMillis) ;
+    }
   }
 }
 
+void initBluetoothServices() {
+  boolean success;
+  SERIALPRINTLN(F("CSC home biker peripheral"));
+  /* Initialise the module */
+  SERIALPRINT(F("Initialising the Bluefruit LE module: "));
+  ERROR_IF_NOT( ble.begin(VERBOSE_MODE) , "Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?" );
+  SERIALPRINTLN( F("OK!") );
 
-int oldRpm = 0;
+#ifdef FACTORYRESET_ENABLE
+  /* Perform a factory reset to make sure everything is in a known state */
+  SERIALPRINTLN(F("Performing a factory reset: "));
+  ERROR_IF_NOT( ble.factoryReset(), "Couldn't factory reset");
+#endif
+  ERROR_IF_NOT( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION), "Callback requires at firmware least 0.7.0" );
 
-void updateCSC(long currentMillis) {
-  /* Read the current voltage level on the A0 analog input pin.
-     This is used here to simulate the heart rate's measurement.
-  */  
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  SERIALPRINTLN("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+#ifdef SERIALDBG
+  ble.info();
+#endif
+
+  ble.setConnectCallback(central_connected);
+  ble.setDisconnectCallback(central_disconnected);
+  ble.setBleUartRxCallback(bleUartRX);
+
+  ble.sendCommandCheckOK(F("AT+HWMODELED=1"));
+
+  ERROR_IF_NOT( ble.sendCommandCheckOK(F("AT+GAPDEVNAME=INBIKE CSC")), "Could not set device name?" );
+
+  cscServiceId = gatt.addService(GATT_CSC_SERVICE_UID);
+  ERROR_IF_ZERO(cscServiceId, "Could not add CSC service")
+
+  /* CSC Measurement characteristic */
+  cscMeasureCharId = gatt.addCharacteristic(GATT_CSC_MEASURE_CHAR_UID, GATT_CHARS_PROPERTIES_NOTIFY, 2, CSCMSIZE, BLE_DATATYPE_BYTEARRAY);
+  ERROR_IF_ZERO(cscMeasureCharId, "Failed adding csc rate measurment characteristic")
+
+  cscFeaturesCharId = gatt.addCharacteristic(GATT_CSC_FEATURE_CHAR_UID, GATT_CHARS_PROPERTIES_READ, 2, 2, BLE_DATATYPE_BYTEARRAY);
+  ERROR_IF_ZERO(cscFeaturesCharId, "Failed adding csc feature char");
+  gatt.setChar(cscFeaturesCharId, featFlagsArray, 2);
+
+  cscSensorLocCharId = gatt.addCharacteristic(GATT_CSC_SENSORLOC_CHAR_ID, GATT_CHARS_PROPERTIES_READ, 1, 1, BLE_DATATYPE_BYTEARRAY);
+  ERROR_IF_ZERO(cscSensorLocCharId, "Failed adding sensor loc char");
+  gatt.setChar(cscSensorLocCharId, locationArray, 1);
+
+  cscServicePointCharId = gatt.addCharacteristic(GATT_CSC_CONTROL_POINT_CHAR_ID, GATT_CHARS_PROPERTIES_WRITE | GATT_CHARS_PROPERTIES_INDICATE, 1, 32, BLE_DATATYPE_BYTEARRAY);
+  ERROR_IF_ZERO(cscServicePointCharId, "Failed adding control point char");
+
+
+  /* Add the csc uuid to the advertising data (needed for Nordic apps to detect the service) */
+  advdataArray[ADVLOWIDPOS] = GATT_CSC_SERVICE_UID_LOWBYTE;
+  advdataArray[ADVHIGHIDPOS] = GATT_CSC_SERVICE_UID_HIGHBYTE;
+  ble.setAdvData( advdataArray, sizeof(advdataArray) );
+
+  //set connectable state for device
+  ERROR_IF_NOT( ble.sendCommandCheckOK(F("AT+GAPCONNECTABLE=1")), "Could not set connectable mode" );
+
+  //start adverising
+  ERROR_IF_NOT( ble.sendCommandCheckOK(F("AT+GAPSTARTADV")), "Could not start adveristing" );
+
+#ifdef ALLEVENTS
+  SERIALPRINT("CSC BLUETOOTH ALLEVENTS Mode ");
+  SERIALPRINTLN(CSCMSIZE);
+#else
+  SERIALPRINT("CSC BLUETOOTH ONLY wheel events Mode ");
+  SERIALPRINTLN(CSCMSIZE);
+#endif
+  /* Reset the device for the new service setting changes to take effect */
+  ble.reset();
+}
+
+void readAndPrintPeerAddress(void) {
+  ble.println("AT+BLEGETPEERADDR");
+  if (! ble.waitForOK() ) {
+    SERIALPRINTLN(F("Failed to execute BLEGETPEERADDR"));
+  }
+  ble.readline();
+}
+
+
+void central_connected(void)
+{
+  is_central_connected = true;
+}
+
+void central_disconnected(void)
+{
+  is_central_connected = false;
+}
+
+void bleGattRX(int32_t chars_id, uint8_t data[], uint16_t len)
+{
+  SERIALPRINT( F("[BLE GATT RX] (" ) );
+  SERIALPRINT(chars_id);
+  SERIALPRINT(") ");
+
+  if (chars_id == cscServicePointCharId)
+  {
+#ifdef SERIALDBG
+    Serial.write(data, len);
+    Serial.println();
+#endif
+  }
+  //  else if (chars_id == charid_number)
+  //  {
+  //    int32_t val;
+  //    memcpy(&val, data, len);
+  //    Serial.println(val);
+  //  }
+}
+
+
+void bleUartRX(char data[], uint16_t len)
+{
+#ifdef SERIALDBG
+  Serial.print( F("[BLE UART RX]" ) );
+  Serial.write(data, len);
+  Serial.println();
+#endif
+}
+
+void initLEDPort() {
+  ledport = digitalPinToPort(LED_BUILTIN);
+  ledbit = digitalPinToBitMask(LED_BUILTIN);
+}
+
+void blinkLED(int cnt) {
+  for (int i = 0; i < cnt; i++) {
+    (*ledport) = (*ledport) | ledbit ;
+    delay(200);                       // wait for a second
+    (*ledport) = (*ledport) & ~ledbit;
+    delay(300);
+  }
+}
+
+void setLED(boolean state) {
+  if (state)
+  {
+    (*ledport) = (*ledport) | ledbit ;
+  } else {
+    (*ledport) = (*ledport) & ~ledbit;
+  }
+
+}
+
+
+void updateCSCviaBLE(long currentMillis) {
+
   unsigned long mltime = currentMillis % (65536);
 
-
-  currCrankPos += 1; //fake
-
+  currCrankPos += 3; //fake
+  if (updating) {
+    delay(1);
+  }
   unsigned char n24 = (unsigned char)(currWheelPos >> 24);
   unsigned char n16 = (unsigned char)(currWheelPos >> 16);
   unsigned char n8 = (unsigned char)(currWheelPos >> 8);
@@ -159,7 +292,7 @@ void updateCSC(long currentMillis) {
   unsigned char c8 = (unsigned char)(currCrankPos >> 8);
   unsigned char c0 = (unsigned char) currCrankPos;
 
-
+#ifdef SERIALDBG
   Serial.print(mltime);
   Serial.print(" ");
   Serial.print(m8);
@@ -178,8 +311,8 @@ void updateCSC(long currentMillis) {
   Serial.print(" ");
   Serial.print(c0);
   Serial.println();
+#endif
 
- 
 #ifdef ALLEVENTS
   const unsigned char rpmCharArray[CSCMSIZE] = { CSC_WHEEL_REV_PRESENT | CSC_CRANK_REV_PRESENT  ,
                                                  n0, n8, n16, n24, m0, m8,
@@ -190,55 +323,30 @@ void updateCSC(long currentMillis) {
                                                  n0, n8, n16, n24, m0, m8
                                                };
 #endif
-  cscMeasureChar.setValue(rpmCharArray, CSCMSIZE);  // and update the heart rate measurement characteristic  
-  
-  //oldRpm = rpm;           // save the level for next comparison
-  //}
+  gatt.setChar(cscMeasureCharId, rpmCharArray, CSCMSIZE);
+
 }
 
 
-void blinkLED(int cnt) {
-  for (int i = 0; i < cnt; i++) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(200);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(300);
-  }
-}
-
-
-void cscSCControlPointCharWritten(BLEDevice tcentral, BLECharacteristic tcharacteristic) {
-  // central wrote new value to characteristic, update LED
-  Serial.print("Characteristic event, written: ");
-  //
-  //  if (switchChar.value()) {
-  //    Serial.println("LED on");
-  //    digitalWrite(ledPin, HIGH);
-  //  } else {
-  //    Serial.println("LED off");
-  //    digitalWrite(ledPin, LOW);
-  //  }
-}
-
-void do_wcount() {
+void doWheelCount() {
   if (isodd) {
     isodd = false;
-    digitalWrite(LED_BUILTIN, HIGH);
   } else {
-    digitalWrite(LED_BUILTIN, LOW);
     isodd = true;
   }
-  if(slice>=2){
-    slice=0;
-    updating=true;
+  if (slice >= 4) {
+    slice = 0;
+    updating = true;
     currWheelPos += 1;
-    updating=false;
-  }else{
-    slice+=1;
+    updating = false;
+  } else {
+    slice += 1;
   }
 
-  
+  setLED(isodd);
+
 
 }
+
 
 
---
+++ bluefruitconfig.h
@@ -0,0 +1,76 @@
+// COMMON SETTINGS
+// ----------------------------------------------------------------------------------------------
+// These settings are used in both SW UART, HW UART and SPI mode
+// ----------------------------------------------------------------------------------------------
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+
+// SOFTWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins that will be used for 'SW' serial.
+// You should use this option if you are connecting the UART Friend to an UNO
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SWUART_RXD_PIN       9    // Required for software serial!
+#define BLUEFRUIT_SWUART_TXD_PIN       10   // Required for software serial!
+#define BLUEFRUIT_UART_CTS_PIN         11   // Required for software serial!
+#define BLUEFRUIT_UART_RTS_PIN         8    // Optional, set to -1 if unused
+
+
+// HARDWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the HW serial port you are using. Uncomment
+// this line if you are connecting the BLE to Leonardo/Micro or Flora
+// ----------------------------------------------------------------------------------------------
+#ifdef Serial1    // this makes it not complain on compilation if there's no Serial1
+  #define BLUEFRUIT_HWSERIAL_NAME      Serial1
+#endif
+
+
+// SHARED UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following sets the optional Mode pin, its recommended but not required
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_UART_MODE_PIN        12    // Set to -1 if unused
+
+
+// SHARED SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for HW and SW SPI communication.
+// SCK, MISO and MOSI should be connected to the HW SPI pins on the Uno when
+// using HW SPI.  This should be used with nRF51822 based Bluefruit LE modules
+// that use SPI (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_CS               8
+#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+
+// SOFTWARE SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for SW SPI communication.
+// This should be used with nRF51822 based Bluefruit LE modules that use SPI
+// (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_SCK              13
+#define BLUEFRUIT_SPI_MISO             12
+#define BLUEFRUIT_SPI_MOSI             11
+
+
+// CSC SERVICES IDS
+//CSC = Cycling Speed and Cadence
+#define FLAGS_SIZE 1
+#define WHEEL_REV_SIZE 4
+#define CRANK_REV_SIZE 2
+#define TIME_EVENT_SIZE 2
+#define GATT_CSC_SERVICE_UID 0x1816
+#define GATT_CSC_SERVICE_UID_LOWBYTE 0x16
+#define GATT_CSC_SERVICE_UID_HIGHBYTE 0x18
+#define GATT_CSC_MEASURE_CHAR_UID 0x2A5B
+#define GATT_CSC_FEATURE_CHAR_UID 0x2A5C
+#define GATT_CSC_SENSORLOC_CHAR_ID 0x2A5D
+#define GATT_CSC_CONTROL_POINT_CHAR_ID 0x2A55
+
+#define CSC_WHEEL_REV_PRESENT 1
+#define CSC_CRANK_REV_PRESENT 2
+
+
